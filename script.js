48ad76f81098b7aa9178f8112cf578ddf627640f
omatography <omkarslam420@gmail.com>
2020-11-08 16:22:33 +0530

    Added Javascript animations

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(() =>{
+
+    $(window).scroll(() =>{
+        if($(window).scrollTop() > 120){
+            $('#navbar').addClass("dark-bg");
+        }else{
+            $('#navbar').removeClass("dark-bg");
+        }
+    });
+    $('#navbar').addClass(" animate__animated animate__fadeInDown animate__fast");
+    $('#home-content').addClass(" animate__animated animate__fadeIn animate__fast");
+    
+    $('#about').waypoint(function(direction){
+        $('#about').addClass('animate__animated animate__fadeIn animate__fast ');
+    
+        },{
+            offset: '80%'
+    });
+
+    $('#services').waypoint(function(direction){
+        $('#services').addClass('animate__animated animate__fadeIn animate__fast ');
+    
+        },{
+            offset: '70%'
+    });
+
+    $('#portfolio').waypoint(function(direction){
+        $('#portfolio').addClass('animate__animated animate__fadeIn animate__fast');
+    
+        },{
+            offset: '70%'
+    });
+
+    $('#contacts').waypoint(function(direction){
+        $('#contacts').addClass('animate__animated animate__fadeIn animate__fast');
+    
+        },{
+            offset: '70%'
+    });
+
+    $('#test').waypoint(function(direction){
+        $('#testl').addClass('animate__animated animate__fadeInLeft animate__fast');
+        $('#testr').addClass('animate__animated animate__fadeInRight animate__fast');
+    
+        },{
+            offset: '70%'
+    });
+
+});
